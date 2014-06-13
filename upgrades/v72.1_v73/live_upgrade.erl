1b859f057eff24ebe7910e59d1377bcfeb1a8e73
Alex Arnell <alex@heroku.com>
2014-06-13 15:20:12 -0700

    adding live_upgrade script

---
+++ upgrades/v72.1_v73/live_upgrade.erl
@@ -0,0 +1,71 @@
+% Run this once per cluster to create the firehose channel you will need to
+% record the returned ChannelId for use in the UpgradeNode fun.
+f(RunOnce).
+RunOnce = fun() ->
+    ChannelId = logplex_channel:create_id(),
+    not is_integer(ChannelId) andalso exit({expected_integer, ChannelId}),
+    Token = logplex_token:create(ChannelId, <<"lumbermill-firehose">>),
+
+    [{channel_id, ChannelId}, {token, Token}]
+end.
+
+f(UpgradeNode).
+UpgradeNode = fun () ->
+    CurVsn = "v72.1",
+    NextVsn = "v73",
+    case logplex_app:config(git_branch) of
+        CurVsn ->
+            io:format(whereis(user), "at=upgrade_start cur_vsn=~p~n", [CurVsn]);
+        NextVsn ->
+            io:format(whereis(user),
+                      "at=upgrade type=retry cur_vsn=~p old_vsn=~p~n", [CurVsn, NextVsn]);
+        Else ->
+            io:format(whereis(user),
+                      "at=upgrade_start old_vsn=~p abort=wrong_version", [tl(Else)]),
+            erlang:error({wrong_version, Else})
+    end,
+
+    %% Stateless
+
+    {module, logplex_firehose} = l(logplex_firehose),
+    {module, logplex_app} = l(logplex_app),
+    {module, logplex_channel} = l(logplex_channel),
+    {module, logplex_message} = l(logplex_message),
+    {module, logplex_worker} = l(logplex_worker),
+
+    ok = application:set_env(logplex, firehose_channel_id, erlang:error("replace with the int channel ID")),
+
+    io:format(whereis(user), "at=upgrade_end cur_vsn=~p~n", [NextVsn]),
+    ok = application:set_env(logplex, git_branch, NextVsn),
+    ok
+end.
+
+f(NodeVersions).
+NodeVersions = fun () ->
+                       lists:keysort(3,
+                           [ {N,
+                              element(2, rpc:call(N, application, get_env, [logplex, git_branch])),
+                              rpc:call(N, os, getenv, ["INSTANCE_NAME"])}
+                             || N <- [node() | nodes()] ])
+               end.
+
+f(NodesAt).
+NodesAt = fun (Vsn) ->
+                  [ N || {N, V, _} <- NodeVersions(), V =:= Vsn ]
+          end.
+
+
+f(RollingUpgrade).
+RollingUpgrade = fun (Nodes) ->
+  lists:foldl(fun (N, {good, Upgraded}) ->
+    case rpc:call(N, erlang, apply, [ UpgradeNode, [] ]) of
+      ok ->
+        {good, [N | Upgraded]};
+      Else ->
+        {{bad, N, Else}, Upgraded}
+    end;
+    (N, {_, _} = Acc) -> Acc
+    end,
+    {good, []},
+    Nodes)
+end.
