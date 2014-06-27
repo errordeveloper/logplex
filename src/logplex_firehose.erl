ad030cc462f55c3743b4618f2761dd76fed24c11
Alex Arnell <alex@heroku.com>
2014-06-27 10:55:44 -0700

    performance enhancements
    - global lock free hash generation
    - trade a larger ets table for two ets reads
    
    The latter enhancement trades memory for an extra ets lookup. This
    leaves more memory available for messages and drains. The two ets
    lookups are performed on different tables to increase concurrency.
    
    Futher optimizations could be made by reducing the firehose stat
    increments.
    
    Credits go to @ferd for these suggestions.

--- src/logplex_firehose.erl
+++ src/logplex_firehose.erl
@@ -14,7 +14,7 @@
 %%
 %% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 %% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-%% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+%% OF MERCHANLOOKUP_TABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 %% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
 %% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 %% WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
@@ -25,15 +25,18 @@
 -include("logplex.hrl").
 -include("logplex_logging.hrl").
 
--define(TAB, firehose).
+-define(LOOKUP_TAB, firehose_master).
+-define(WORKER_TAB, firehose_workers).
 -define(MASTER_KEY, master_shard).
 
--export([lookup_shard/1,
-         next_shard/1,
-         post_msg/3]).
+-export([post_msg/3]).
 
 -export([create_ets_table/0,
+         lookup_shard/1,
+         next_shard/1,
          read_and_store_master_info/0]).
+
+-export([next_hash/1]).
 
 -record(shard_pool, {key :: ?MASTER_KEY | logplex_channel:id(),
                      size=0 :: integer(),
@@ -44,18 +47,15 @@
 %%%--------------------------------------------------------------------
 
 create_ets_table() ->
-    ets:new(?TAB, [named_table, public, set,
+    ets:new(?LOOKUP_TAB, [named_table, public, set,
                               {keypos, #shard_pool.key},
+                              {read_concurrency, true}]),
+    ets:new(?WORKER_TAB, [named_table, public, set,
+                              {keypos, 1},
                               {read_concurrency, true}]).
 
-lookup_shard(ChannelId) when is_integer(ChannelId) ->
-    case ets:lookup(?TAB, ChannelId) of
-        [] -> update_shard_pool(ChannelId);
-        [Shard] -> Shard
-    end.
-
 next_shard(ChannelId) when is_integer(ChannelId) ->
-    select_random(lookup_shard(ChannelId)).
+    lookup_shard(next_hash(ChannelId)).
 
 post_msg(SourceId, TokenName, Msg)
   when is_integer(SourceId),
@@ -69,56 +69,47 @@ post_msg(SourceId, TokenName, Msg)
     end.
 
 read_and_store_master_info() ->
+    %% Ids = firehose_channel_ids(),
+    %% Seed = [ Id || _ <- lists:seq(1, length(Ids)), Id <- Ids ],
+    Seed = firehose_channel_ids(),
+    ets:insert(?LOOKUP_TAB,
+               #shard_pool{key=?MASTER_KEY, size=length(Seed), pool=Seed }),
+    store_channels(1, Seed),
+    ok.
+
+store_channels(_Index, []) ->
+    ok;
+store_channels(Index, [Id | Rest]) ->
+    ets:insert(?WORKER_TAB, {Index, Id}),
+    store_channels(Index+1, Rest).
+
+firehose_channel_ids() ->
     case logplex_app:config(firehose_channel_ids, []) of
-        ShardPool=#shard_pool{ key=?MASTER_KEY } ->
-            ets:delete_all_objects(?TAB),
-            true = ets:insert(?TAB, ShardPool),
-            ShardPool;
+        [] -> [];
         Ids when is_list(Ids) ->
             ChannelIdStrings = string:tokens(logplex_app:config(firehose_channel_ids, ""), ","),
-            ChannelIds = list_to_tuple([ list_to_integer(Id) || Id <- ChannelIdStrings ]),
-            application:set_env(logplex, firehose_channel_ids,
-                                #shard_pool{ key=?MASTER_KEY,
-                                             size=size(ChannelIds),
-                                             pool=ChannelIds}),
-            read_and_store_master_info()
+            [ list_to_integer(Id) || Id <- ChannelIdStrings ]
     end.
 
 %%%--------------------------------------------------------------------
 %%% private functions
 %%%--------------------------------------------------------------------
 
-clone_shard_pool(Key, Orig=#shard_pool{pool=Pool}) ->
-    case is_member(Key, Pool) of
-        true ->
-            #shard_pool{key=Key};
-        false ->
-            Orig#shard_pool{ key=Key }
-    end.
+compute_hash(_, 0) ->
+    0;
+compute_hash(ChannelId, Bounds) ->
+    erlang:phash2({os:timestamp(), self(), ChannelId}, Bounds) + 1.
 
-is_member(Id, Tuple) when is_tuple(Tuple) ->
-    is_member(Id, Tuple, 1).
-
-is_member(_Id, Tuple, Pos) when Pos > size(Tuple) ->
-    false;
-is_member(Id, Tuple, Pos) ->
-    case element(Pos, Tuple) =:= Id of
-        true -> true;
-        false -> is_member(Id, Tuple, Pos+1)
-    end.
-
-select_random(#shard_pool{ size=0 }) ->
+lookup_shard(0) ->
     undefined;
-select_random(#shard_pool{ size=Size, pool=Pool}) ->
-    Pos = erlang:phash2({make_ref(), self()}, Size) + 1,
-    erlang:element(Pos, Pool).
+lookup_shard(ShardId) when is_integer(ShardId) ->
+    try ets:lookup_element(?WORKER_TAB, ShardId, 2) of
+        ChannelId -> ChannelId
+    catch error:badarg -> undefined
+    end.
 
-update_shard_pool(ChannelId) when is_integer(ChannelId) ->
-    ShardPool = case ets:lookup(?TAB, ?MASTER_KEY) of
-        [MasterInfo] -> clone_shard_pool(ChannelId, MasterInfo);
-        [] ->
-            clone_shard_pool(ChannelId, read_and_store_master_info())
-    end,
-    true = ets:insert(?TAB, ShardPool),
-    ShardPool.
-
+next_hash(ChannelId) ->
+    try ets:lookup_element(?LOOKUP_TAB, ?MASTER_KEY, #shard_pool.size) of
+        Num -> compute_hash(ChannelId, Num)
+    catch error:badarg -> 0
+    end.
